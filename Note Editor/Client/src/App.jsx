cadf7cf34de738e1980a9c0962afcd1ea13e2f15
Lm09 <105072858+Lagnajit09@users.noreply.github.com>
2024-04-16 20:16:07 +0530

    Added Note Editor Frontend using Tiptap.js

---
+++ Note Editor/Client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TipTap from "./TipTap";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <TipTap />
+    </div>
+  );
+};
+
+export default App;
