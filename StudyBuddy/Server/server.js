f1e1e0e0ef7d2b30fa88dea21e48f531e8848e4b
Lm09 <123.moharana@gmail.com>
2024-06-22 01:22:33 +0530

    refactored server socket.io

--- StudyBuddy/Server/server.js
+++ StudyBuddy/Server/server.js
@@ -12,7 +12,6 @@ const chatRouter = require("./Chatroom/chat");
 const communityRouter = require("./Chatroom/community");
 require("dotenv").config();
 const cors = require("cors");
-const { handleSocketConnection } = require("./Chatroom/socketHandler");
 const geminiRouter = require("./Gemini/gemini");
 const signupSchema = require("./User/signup-Schema");
 const validate = require("./User/validate-signup");
@@ -31,18 +30,7 @@ connectDb().then(() => {
   console.log("Hello DB");
 });
 
-//socket-io cors configuration
-const io = require("socket.io")(server, {
-  cors: {
-    origin: ["https://studybuddy-52816.web.app", "http://localhost:5173", "*"],
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
-
 //Use the Routers
-
-//To handle User
 app.route("/signup").post(validate(signupSchema), auth.signup); //route to signup page //Validation to be added
 app.route("/login").post(auth.login); //route to handle login page
 app.use("/", oAuthRouter);
@@ -52,9 +40,6 @@ app.use("/courses", geminiRouter);
 app.use("/note", noteRouter);
 app.use("/chatroom/chat", chatRouter);
 app.use("/chatroom/community", communityRouter);
-
-//connect to socket.io
-handleSocketConnection(io);
 
 //Start Server
 const PORT = process.env.PORT || 3000;
